317940b0e641b9a0f3139faefe735a85264769b2
Felipe Okino <felipe1okino@live.com>
2023-10-20 21:23:13 -0300

    feat: improve experience and running server steps

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Authenticated from './components/Authenticated';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import NotFound from './components/404/NotFound';
 
 function App() {
   return (
-     <BrowserRouter basename='/'>
+     <BrowserRouter basename='/' >
       <div className="App w-screen h-screen flex justify-center items-center text-white bg-zinc-950">
         <Routes>
           <Route path='/login' Component={Login} />
           <Route path='/' Component={Authenticated(Home)} />
+          <Route path='*' Component={NotFound} />
+
         </Routes>
       </div>
      </BrowserRouter>
